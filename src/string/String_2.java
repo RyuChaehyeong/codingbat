326e9c943828e1936c86caf1765a6bc01cf9e061
RyuChaehyeong <fluid15@naver.com>
2021-03-05 13:55:25 +0900

    string

--- src/string/String_2.java
+++ src/string/String_2.java
@@ -33,12 +33,66 @@ public class String_2 {
 
 		for (int i = 0; i < str.length() - 2; i++) {
 
-			if (str.substring(i, i + 3).equals("cat")) {cnt_cat++;}
+			if (str.substring(i, i + 3).equals("cat")) {
+				cnt_cat++;
+			}
 
-			if (str.substring(i, i + 3).equals("dog")) {cnt_dog++;}
+			if (str.substring(i, i + 3).equals("dog")) {
+				cnt_dog++;
+			}
 		}
 
 		return cnt_cat == cnt_dog;
+	}
+
+	public int countCode(String str) {
+		int cnt = 0;
+		for (int i = 0; i < str.length() - 3; i++) {
+			if (str.substring(i, i + 2).equals("co") && str.charAt(i + 3) == 'e') {
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+
+	public boolean endOther(String a, String b) {
+		String up_a = a.toUpperCase();
+		String up_b = b.toUpperCase();
+
+		return up_a.endsWith(up_b) || up_b.endsWith(up_a);
+	}
+
+	// **
+	public boolean xyzThere(String str) {
+		str = " " + str;
+		for (int i = 0; i < str.length() - 2; i++) {
+			if (str.substring(i, i + 3).equals("xyz") && str.charAt(i - 1) != '.')
+				return true;
+		}
+
+		return false;
+	}
+
+	// 수정전: 이렇게 하면 반복문 한번에 메소드 끝나...
+	public boolean bobThere_before(String str) {
+		for (int i = 0; i < str.length() - 2; i++) {
+			String subs = str.substring(i, i + 3);
+			return subs.charAt(0) == 'b' && subs.endsWith("b");
+		}
+
+		return false;
+	}
+
+	// 수정후
+	public boolean bobThere_after(String str) {
+		for (int i = 0; i < str.length() - 2; i++) {
+			String subs = str.substring(i, i + 3);
+			if (subs.charAt(0) == 'b' && subs.endsWith("b")) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 }
